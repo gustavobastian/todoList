8b347476f1f428466febd498705d0cc90cfe3108
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-03-30 17:02:37 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+function component(){
+    let contentElement=document.createElement('div') 
+    let navBar= document.createElement('div'); 
+    let snippetLocal = document.createTextNode("ToDo List");
+    navBar.appendChild(snippetLocal);
+    contentElement.appendChild(navBar);
+    return contentElement;
+}
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  mode:'development', 
+  entry: {
+    index:{
+      import:'./src/index.js',
+      }
+      ,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'ToDoList',
+    }),
+  ],
+  output: {
+    //filename: 'main.js',
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
