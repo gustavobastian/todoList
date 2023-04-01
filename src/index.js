6132a4b9465ca3ea541e009f8631f6ddefafdc8c
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-04-01 12:18:42 -0300

    generating page layout

--- src/index.js
+++ src/index.js
@@ -7,7 +7,25 @@ function component(){
     let navBar= document.createElement('div'); 
     let snippetLocal = document.createTextNode("ToDo List");
     navBar.appendChild(snippetLocal);
+    navBar.className="header";
     contentElement.appendChild(navBar);
+    //dinamic content
+    
+    let mainContent= document.createElement('div'); 
+    let snippetLocal2 = document.createTextNode("ToDo List");
+    mainContent.className="dinamicContent";
+    mainContent.appendChild(snippetLocal2);
+    contentElement.appendChild(mainContent);
+
+    //generating footer
+
+    let footerContent=document.createElement('div');
+    footerContent.className="textFooter";
+    let snippetFooter = document.createTextNode("By Gustavo Bastian (2023)");       
+    footerContent.appendChild(snippetFooter);
+    contentElement.appendChild(footerContent);
+
+
     return contentElement;
 }
 document.body.appendChild(component());
